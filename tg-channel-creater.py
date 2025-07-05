31c48521b5ba4f89f6e7f728897d00214bd53f32
yourunb <bxyura@gmail.com>
2025-07-05 10:51:33 +0300

    feat: add args and loop for check channel

--- tg-channel-creater.py
+++ tg-channel-creater.py
@@ -1,24 +1,36 @@
+import sys
 from telethon.sync import TelegramClient
 from telethon.tl.functions.channels import CreateChannelRequest, CreateForumTopicRequest
 
 api_id = 11111111
-api_hash = '111aaa111aaa111aaa111aaa'
+api_hash = '11111111aaaaaaaaaaaa'
 
 client = TelegramClient('session_name', api_id, api_hash)
 
-tour_name = "Сёрф-Кемп"
-date = "02.07.2025"
+tour_name = sys.argv[1]
+date = sys.argv[2]
 group_title = f"{tour_name} {date}"
+
+async def channel_exists(title):
+  async for dialog in client.iter_dialogs():
+    if dialog.is_channel and dialog.name == title:
+      return True
+  return False
 
 async def main():
   await client.start()
-  # cоздание форум-группы
+
+  if await channel_exists(group_title):
+    print(f"Группа '{group_title}' уже существует. Создание отменено.")
+    return
+
   result = await client(CreateChannelRequest(
     title=group_title,
     about="Группа тура",
     megagroup=True,
     forum=True
   ))
+
   group = result.chats[0]
   
   topics = [
@@ -37,5 +49,4 @@ async def main():
     ))
 
 with client:
-  client.loop.run_until_complete(main())
-
+    client.loop.run_until_complete(main())
